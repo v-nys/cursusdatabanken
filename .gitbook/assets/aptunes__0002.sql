0bbad3070f7914c0d7a2bc7cca154491caff78d2
Vincent Nys <vincentnys@gmail.com>
2020-10-22 06:43:13 +0000

    GitBook: [master] no pages modified

--- .gitbook/assets/aptunes__0002.sql
+++ .gitbook/assets/aptunes__0002.sql
@@ -1,14 +1,14 @@
 insert into Nummers(Titel,Artiest,Genre,Album,Duurtijd,ReleaseDatum)
 values
-('Clair de Lune','Claude Debussy','Klassiek','Verzameld werk van Debussy',75,1890),
-('Suite bergamesqu','Claude Debussy','Klassiek','Verzameld werk van Debussy',160,1890),
+('Clair de Lune','Claude Debussy','Klassiek','Verzameld werk van Debussy',75,'1890-01-01'),
+('Suite bergamesqu','Claude Debussy','Klassiek','Verzameld werk van Debussy',160,'1890-01-01'),
 ('Rêverie','Claude Debussy','Klassiek','Verzameld werk van Debussy',90,NULL),
-('Stairway to Heaven','Led Zeppelin','Blues rock','Led Zeppelin IV',420,1971),
-('When the Levee Breaks','Led Zeppelin','Blues','Led Zeppelin IV',320,1971),
+('Stairway to Heaven','Led Zeppelin','Blues rock','Led Zeppelin IV',420,'1971-01-01'),
+('When the Levee Breaks','Led Zeppelin','Blues','Led Zeppelin IV',320,'1971-01-01'),
 ('Achilles'' Last Stand','Led Zeppelin','Rock','Presence',500,NULL),
-('Little Sun','Blues Pills','Blues rock','Blues Pills',300,2014),
-('High Class Woman','Blues Pills','Blues rock','Blues Pills',300,2014),
-('Little Boy Preacher','Blues Pills','Blues rock','Lady in Gold',300,2016),
+('Little Sun','Blues Pills','Blues rock','Blues Pills',300,'2014-01-01'),
+('High Class Woman','Blues Pills','Blues rock','Blues Pills',300,'2014-01-01'),
+('Little Boy Preacher','Blues Pills','Blues rock','Lady in Gold',300,'2016-01-01'),
 ('We made it','Ghostface Killah','Rap','Supreme Clientele',277,NULL),
-('Saturday Nite','Ghostface Killah','Hiphop','Supreme Clientele',99,2000),
+('Saturday Nite','Ghostface Killah','Hiphop','Supreme Clientele',99,'2000-01-01'),
 ('The Abyss','Sepultura','Death metal','Schizophrenia',240,NULL);
